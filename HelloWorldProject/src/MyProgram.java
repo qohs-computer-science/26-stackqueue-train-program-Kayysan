860edb5147cae74c6bb22f728b1f8ad69242e023
Kayysan <rightkaysan@gmail.com>
2025-01-24 04:16:13 +0000

    prog

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -15,6 +15,21 @@ public class MyProgram {
 	public static void main(String[] args) {
 
 		int limitTrackA = 100000, limitTrackB = 100000, limitTrackC = 100000;
+		Stack <Train> all = new LinkedList <Train> (); 
+		Stack <Train> trackA = new LinkedList <Train> ();
+		Stack <Train> trackB = new LinkedList <Train> ();
+		Stack <Train> trackC = new LinkedList <Train> ();
+		Stack <Train> trackD = new LinkedList <Train> ();
+
+		int engLim= 10000;
+		
+		int aWeight = 0;
+		int bWeight = 0;
+		int cWeight = 0;
+		int dWeight = 0;
+
+		Queue <Train> cars = new LinkedList<Train>();
+ 
 	
 		Scanner x = new Scanner(System.in);
 		try{
@@ -27,5 +42,122 @@ public class MyProgram {
 		{
 			System.out.println(e.getMessage());
 		}
+
+
+
+		String t=x.nextLine();
+
+
+		while(!t.equals("END")){
+			if (name.substring(0,3).equals("CAR")){
+				cars.add(new Train(t, x.nextLine(),x.nextLine(),x.nextLine(),x.nextLine(),x.nextLine()));
+				t=x.nextLine();
+				t=x.nextLine();
+			}
+			else{
+				cars.add(new Train(t,x.nextLine()));
+				t=x.nextLine();
+			}
+		}// end while
+
+		while(cars.size()>0){
+			Train temp=carlist.remove();
+			if(temp.getMiles()<=700){
+				if ((temp.getDestination().equals("Baltimore"))){
+					cWeight+=temp.getWeight();
+					if (cWeight<limitTrackC){
+						trackC.push(temp);
+					}
+					else{
+						cWeight-=temp.getWeight();
+						trackC.push(new Train("ENG00000", "Baltimore"));
+
+						System.out.println(trackC.toString());
+						
+						while (trackC.size>0){
+							Train tempCar=trackC.pop();
+							System.out.println(tempCar.getName()+" contains the product: "+ tempCar.getProduct());
+						}
+						cWeight=0;
+					}
+
+				}//end balt
+				else if ((temp.getDestination().equals("Charlotte"))){
+				bWeight+=temp.getWeight();
+					if (bWeight<limitTrackB){
+						trackB.push(temp);
+					}
+					else{
+						bWeight-=temp.getWeight();
+						trackB.push(new Train("ENG00000", "Charlotte"));
+
+						System.out.println(trackB.toString());
+						
+						while (trackB.size>0){
+							Train tempCar=trackB.pop();
+							System.out.println(tempCar.getName()+" contains the product: "+ tempCar.getProduct());
+						}
+						bWeight=0;
+					}
+
+				}//end Char
+				else if ((temp.getDestination().equals("Trenton"))){
+					aWeight+=temp.getWeight();
+					if (aWeight<limitTrackA){
+						trackA.push(temp);
+					}
+					else{
+						aWeight-=temp.getWeight();
+						trackA.push(new Train("ENG00000", "Charlotte"));
+
+						System.out.println(trackA.toString());
+						
+						while (trackA.size>0){
+							Train tempCar=trackA.pop();
+							System.out.println(tempCar.getName()+" contains the product: "+ tempCar.getProduct());
+						}
+						aWeight=0;
+					}
+
+				}//end Tren
+
+
+			}//ed big if
+
+		}//end big while
+
+
+		
+
+
+
+
+
+		if(getName().equals("Baltinore")){
+			train();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+
+
+
+
 	}
 }
+
+	
+
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -1,6 +1,51 @@
+//Kaysan Antoni pd3 Adams
 public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
+
+
+    public Train ( String n, String p, String o, String d, int w, int m){
+
+   
+        n=name;
+        p=product;
+        o=origin;
+        d=destination;
+        w=weight;
+        m=miles;
+
+    }
+
+    public Train ( String n, String d){
+        n=name;
+        d=destination;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getProduct(){
+        return product;
+    }
+    public String getOrigin(){
+        return origin;
+    }
+    public String getDestination(){
+        return destination;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public int getMiles(){
+        return miles;
+    }
+
+
+    public toString(){
+        return(name+" leaving for "+ destination+ " with the following cars: ");
+    }
+
+    
 
 
 }
